96c20be8b650e921db3b48bf838bb3f527d9e93d
mk74901n <mk74901n@pace.edu>
2023-12-09 22:19:23 -0500

    final code

---
+++ app/src/main/java/com/example/android/dagger/storage/Storage.kt
@@ -0,0 +1,7 @@
+
+package com.example.android.dagger.storage
+
+interface Storage {
+    fun setString(key: String, value: String)
+    fun getString(key: String): String
+}
---
+++ app/src/test/java/com/example/android/dagger/settings/SettingsViewModelTest.kt
@@ -0,0 +1,37 @@
+
+package com.example.android.dagger.settings
+
+import com.example.android.dagger.user.UserDataRepository
+import com.example.android.dagger.user.UserManager
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+
+class SettingsViewModelTest {
+
+    private lateinit var userManager: UserManager
+    private lateinit var userDataRepository: UserDataRepository
+    private lateinit var viewModel: SettingsViewModel
+
+    @Before
+    fun setup() {
+        userManager = mock(UserManager::class.java)
+        userDataRepository = mock(UserDataRepository::class.java)
+        viewModel = SettingsViewModel(userDataRepository, userManager)
+    }
+
+    @Test
+    fun `Refresh notifications works as expected`() {
+        viewModel.refreshNotifications()
+
+        verify(userDataRepository).refreshUnreadNotifications()
+    }
+
+    @Test
+    fun `Logout works as expected`() {
+        viewModel.logout()
+
+        verify(userManager).logout()
+    }
+}
